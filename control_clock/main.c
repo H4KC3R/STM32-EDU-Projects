73a7f4d842e83eca792b0361cb7738f7a03cd389
Радмир Даминов <daminov.r@phystech.edu>
2023-01-28 20:44:36 +0300

    control_block added

---
+++ control_clock/main.c
@@ -0,0 +1,24 @@
+#include "stm32f4xx_ll_rcc.h"
+#include "stm32f4xx_flash.h"
+#include "stm32f4xx_ll_system.h"
+#include "stm32f4xx_ll_bus.h"
+#include "stm32f4xx_ll_gpio.h"
+
+
+static void rcc_config() {
+	LL_FLASH_SetLatency(LL_FLASH_LATENCY_0);
+
+}
+
+int main(void) {
+	
+	
+	while (1) {
+		
+	}
+	
+	
+}
+
+
+
