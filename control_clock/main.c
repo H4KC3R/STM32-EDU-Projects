ecc5d5250ffbce801810e5023354398f864c3440
Радмир Даминов <daminov.r@phystech.edu>
2023-05-02 16:47:48 +0300

    ongoing

--- control_clock/main.c
+++ control_clock/main.c
@@ -1,12 +1,11 @@
-#include "stm32f4xx_ll_rcc.h"
-#include "stm32f4xx_flash.h"
+#include "stm32f4xx_ll_utils.h"
 #include "stm32f4xx_ll_system.h"
 #include "stm32f4xx_ll_bus.h"
 #include "stm32f4xx_ll_gpio.h"
+#include "stm32f4xx_ll_system.h"
 
 
 static void rcc_config() {
-	LL_FLASH_SetLatency(LL_FLASH_LATENCY_0);
 
 }
 
